c77483f180e23cd0eb5af28e339e4f908549840d
Joy <141945444+Joy0839@users.noreply.github.com>
2024-05-01 16:22:17 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -23,5 +23,66 @@ async def reply_forward(message: Message, file_id: int):
         await asyncio.sleep(e.value)
         await reply_forward(message, file_id)
 
+async def media_forward(bot: Client, user_id: int, file_id: int):
+    try:
+        if Config.FORWARD_AS_COPY is False:
+                lazy_file = await bot.copy_message(chat_id=STREAM_LOGS, from_chat_id=Config.DB_CHANNEL,
+                                          message_id=file_id)
 
+
+                lazy_stream = f"{URL}watch/{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+                lazy_download = f"{URL}{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+                
+                fileName = quote_plus(get_name(lazy_file))
+
+                await lazy_file.reply_text(
+                    text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+                    quote=True,
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=lazy_download),  # we download Link
+                                                        InlineKeyboardButton('▶Stream online', url=lazy_stream)]])  # web stream Link
+                )
+                return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                          message_id=file_id, 
+                                          reply_markup=InlineKeyboardMarkup(
+                                            [
+                                                [
+                                                  InlineKeyboardButton("Fast Download", url=lazy_download),
+                                                  InlineKeyboardButton("▶Stream online", url=lazy_stream),
+                                                ],
+                                            ]),
+                                            )
+        elif Config.FORWARD_AS_COPY is False:
+            lazy_file = await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id)
+            lazy_stream = f"{URL}watch/{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+            lazy_download = f"{URL}{str(lazy_file.id)}/{quote_plus(get_name(lazy_file))}?hash={get_hash(lazy_file)}"
+            fileName = quote_plus(get_name(lazy_file))
+            await lazy_file.reply_text(
+                text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+                quote=True,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=lazy_download),  # we download Link
+                                                    InlineKeyboardButton('▶Stream online', url=lazy_stream)]])  # web stream Link
+            )
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id,
+                                              reply_markup=InlineKeyboardMarkup(
+                                            [
+                                                [
+                                                  InlineKeyboardButton("Fast Download", url=lazy_download),
+                                                  InlineKeyboardButton("▶Stream online", url=lazy_stream),
+                                                ],
+                                            ]),
+                                            )
+
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return media_forward(bot, user_id, file_id)
+
+
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
+    await asyncio.sleep(2)
 
