40dc4fce69845b5e72db1e0ebf0680e837afced8
Manali <manalibishnoi@gmail.com>
2024-07-12 21:20:38 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -54,9 +54,10 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             f"**𝘉𝘢𝘵𝘤𝘩 𝘍𝘪𝘭𝘦𝘴 𝘚𝘵𝘰𝘳𝘦𝘥 𝘪𝘯 𝘮𝘺 𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦!**\n\n𝙃𝙚𝙧𝙚 𝙞𝙨 𝙩𝙝𝙚 𝙋𝙚𝙧𝙢𝙖𝙣𝙚𝙣𝙩 𝙇𝙞𝙣𝙠 𝙤𝙛 𝙮𝙤𝙪𝙧 𝙛𝙞𝙡𝙚𝙨: {share_link} \n\n"
             f"𝘑𝘶𝘴𝘵 𝘊𝘭𝘪𝘤𝘬 𝘵𝘩𝘦 𝘭𝘪𝘯𝘬 𝘵𝘰 𝘨𝘦𝘵 𝘺𝘰𝘶𝘳 𝘧𝘪𝘭𝘦𝘴!",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("⚡️ Open Link ⚡️", url=share_link)],
-                 [InlineKeyboardButton("🔊Books Channel", url="https://t.me/ebookschannell"),
-                  InlineKeyboardButton("🍿support group", url="https://t.me/codeconvo")]]
+                [[InlineKeyboardButton("⚡️ Open Link ⚡️", url=share_link)]
+                 #[InlineKeyboardButton("🔊Books Channel", url="https://t.me/ebookschannell"),
+                #  InlineKeyboardButton("🍿support group", url="https://t.me/codeconvo")
+                 ]
             ),
             disable_web_page_preview=True
         )
@@ -141,15 +142,16 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=LazyDeveloperr_{str_to_b64(file_er_id)}"
+        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=kdrama_girl_{str_to_b64(file_er_id)}"
         await editable.edit(
             "**𝘺𝘰𝘶𝘳 𝘍𝘪𝘭𝘦𝘴 𝘚𝘵𝘰𝘳𝘦𝘥 𝘪𝘯 𝘮𝘺 𝘋𝘢𝘵𝘢𝘣𝘢𝘴𝘦!**\n\n"
             f"𝙃𝙚𝙧𝙚 𝙞𝙨 𝙩𝙝𝙚 𝙋𝙚𝙧𝙢𝙖𝙣𝙚𝙣𝙩 𝙇𝙞𝙣𝙠 𝙤𝙛 𝙮𝙤𝙪𝙧 𝙛𝙞𝙡𝙚: {share_link}  \n\n"
             f"𝘑𝘶𝘴𝘵 𝘊𝘭𝘪𝘤𝘬 𝘵𝘩𝘦 𝘭𝘪𝘯𝘬 𝘵𝘰 𝘨𝘦𝘵 𝘺𝘰𝘶𝘳 𝘧𝘪𝘭𝘦...",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("⚡️ Open Link ⚡️", url=share_link)],
-                 [InlineKeyboardButton("🔊Books Channel", url="https://t.me/ebookschannell"),
-                  InlineKeyboardButton("🍿support group", url="https://t.me/codeconvo")]]
+                # [InlineKeyboardButton("🔊Books Channel", url="https://t.me/ebookschannell"),
+                #  InlineKeyboardButton("🍿support group", url="https://t.me/codeconvo")
+                 ]
             ),
             disable_web_page_preview=True
         )
